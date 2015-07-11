d23d1f75d5422a8e6f03d5162971f8b59d92a979
root <root@ip-172-31-22-196.us-west-1.compute.internal>
2015-07-11 11:41:50 +0000

    changes to basictest on server

--- dataextractiontest/codes/basictest.py
+++ dataextractiontest/codes/basictest.py
@@ -4,23 +4,54 @@ import json
 
 myauth = oauthtoken.getbitlyToken()
 
-dataloc='../../data/'
+dataloc='/root/sunil/pythontest/data/'
+resultsloc='/root/sunil/pythontest/results/'
 
 filename='sample_tweets_data.json'
 
+print 'here1'
+
 fin = open(dataloc+filename)
 
-myjson=json.load(fin)
 
-nodeKeys=set([])
+try:
+    myjson=json.loads(fin.read())
+except:
+    print'loading failed!'
+
+
+nodeKeys={}
 
 def getKeys(inpDict):
     '''returns the set of keys'''
-    return set(inpDict.keys())
+    keysare=inpDict.keys()
+    keysare.sort()
+    return ','.join(keysare)
+
+print 'reading done!'
+
+def printNode(inp):
+    print json.dumps(inp,indent=4)
+
+foutdicts=open(dataloc+'actualsdicts.csv','w')
 
 for num,node in enumerate(myjson):
-    if num>100:
-        break
-    nodeKeys.union(getKeys(node))
-    
-print nodeKeys
+    if num>10:
+        pass
+        #break
+    printNode(node)
+    print
+    keyString=getKeys(node)
+    if keyString not in nodeKeys:
+        nodeKeys[keyString]=1
+    else:
+        nodeKeys[keyString]+=1
+    #print 'nodeKeys are',nodeKeys
+foutdicts.close()
+ 
+fout = open(dataloc+'samplekeys.csv','w')
+print 'final list is written to file' 
+for elem,nums in nodeKeys.iteritems():
+    fout.write(str(nums)+',')
+    fout.write(elem+'\n')
+fout.close()
